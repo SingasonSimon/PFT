44a19fd99bf7f70e58528a412eccf31edc7caca9
Singason Simon <swangui@kabarak.ac.ke>
2025-10-08 01:10:34 +0300

    *finished*

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,26 +1,31 @@
-// Basic Flutter widget test for verifying counter increment functionality.
+// This is a basic Flutter widget test.
 //
-// Use WidgetTester from the flutter_test package to interact with widgets, perform gestures, and verify widget properties and values.
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility in the flutter_test package. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ledgerlite/main.dart';
+// UPDATED: Changed the import to use the new project name 'PatoTrack'
+import 'package:PatoTrack/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build the app and trigger a frame.
+    // Build our app and trigger a frame.
+    // UPDATED: Changed the widget name to PatoTrack()
     await tester.pumpWidget(const PatoTrack());
 
-    // Verify that the counter starts at 0.
+    // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame update.
+    // Tap the '+' icon and trigger a frame.
     await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    // Verify that the counter has incremented to 1.
+    // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
